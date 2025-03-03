3d00f6ce155634ffc59fb1a14ec357beed6ddd92
Mykhailo Safronov <isevenpwnz@gmail.com>
2025-03-03 20:11:50 +0200

    solution v2

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from abc import ABC
-from typing import Any, Type
+from typing import Any
 
 
 class IntegerRange:
@@ -12,7 +12,7 @@ class IntegerRange:
 
     def __get__(self, instance: Any, owner: Any) -> int:
         if instance is None:
-            return self  # type: ignore
+            return self
         return getattr(instance, self.protected_name)
 
     def __set__(self, instance: Any, value: Any) -> None:
@@ -55,17 +55,23 @@ class AdultSlideLimitationValidator(SlideLimitationValidator):
 
 
 class Slide:
-    def __init__(
-            self, name: str, limitation_class: Type[SlideLimitationValidator]
-    ) -> None:
+    def __init__(self, *, name: str, limitation_class: Any) -> None:
         self.name: str = name
-        self.limitation_class: Type[SlideLimitationValidator] = (
-            limitation_class
-        )
+        if isinstance(limitation_class, type):
+            if limitation_class is ChildrenSlideLimitationValidator:
+                self.limitation_validator = limitation_class(4, 20, 80)
+            elif limitation_class is AdultSlideLimitationValidator:
+                self.limitation_validator = limitation_class(14, 50, 120)
+            else:
+                self.limitation_validator = limitation_class(0, 0, 0)
+        else:
+            self.limitation_validator = limitation_class
 
     def can_access(self, visitor: Visitor) -> bool:
         try:
-            self.limitation_class(visitor.age, visitor.weight, visitor.height)
+            self.limitation_validator.age = visitor.age
+            self.limitation_validator.weight = visitor.weight
+            self.limitation_validator.height = visitor.height
         except (TypeError, ValueError):
             return False
         return True
